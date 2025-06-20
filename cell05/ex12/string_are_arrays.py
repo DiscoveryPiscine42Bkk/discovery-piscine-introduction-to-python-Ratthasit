1405fcbc0668396737b69473ac2066c052ccedc8
Ratthasit <mark112222579@gmail.com>
2025-06-20 11:27:35 +0000

    do string_are_arrays

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+    sys.exit()
+
+input_str = sys.argv[1]
+
+count_z = input_str.count('z')
+
+if count_z == 0:
+    print("none")
+else:
+    print("z" * count_z)
